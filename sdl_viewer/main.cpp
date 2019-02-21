c59f9cde721453c246476089780d45fee3255404
brettjm <brettjmanning@gmail.com>
2019-02-20 22:37:48 -0600

    Initial Commit

---
+++ sdl_viewer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sdl_viewer
+//
+//  Created by brett-mac on 2/20/19.
+//  Copyright © 2019 brett-mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
